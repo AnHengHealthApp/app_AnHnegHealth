1726bc3206222b904bb024f2dad4e6e7722e53b0
Henry930127 <148032773+Henry930127@users.noreply.github.com>
2025-03-24 00:32:55 +0800

    style:Update the layout of ForgetPasswordFragment
    As title

--- app/src/main/java/com/example/ahhapp/ForgetPasswordFragment.java
+++ app/src/main/java/com/example/ahhapp/ForgetPasswordFragment.java
@@ -1,10 +1,12 @@
 package com.example.ahhapp;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import androidx.fragment.app.Fragment;
+
 public class ForgetPasswordFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
